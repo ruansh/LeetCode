36c4b95c9ddbb5e55bc4dc3f5712177098e31e50
阮帅 <ruanshuai_sx@qiyi.com>
2020-10-26 09:54:43 +0800

    first commit

---
+++ src/com/ruanshuai/test.java
@@ -0,0 +1,13 @@
+package com.ruanshuai;
+
+/**
+ * @author ruanshuai_sx
+ * @date 2020/10/26
+ */
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
